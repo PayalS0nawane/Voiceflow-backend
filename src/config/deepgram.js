db991c50d33ac876aeb60348a664c60963ec376d
Payal Sonawane <sonawanepayal2005@gmail.com>
2026-02-08 05:22:14 +0530

    testing deepgram key

--- src/config/deepgram.js
+++ src/config/deepgram.js
@@ -1,5 +1,5 @@
 import { createClient } from "@deepgram/sdk";
 
 const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
-
+console.log(process.env.DEEPGRAM_API_KEY ? "found":"missing");
 export default deepgram;
