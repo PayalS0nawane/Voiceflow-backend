3f32596ccee2bc76d052d8f14266e934ebd59097
Payal Sonawane <sonawanepayal2005@gmail.com>
2026-02-19 13:41:07 +0530

    changed cors

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ import speechRoutes from "./routes/speech.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"*",
+  })
+);
 app.use(express.json());
 app.use("/api/speech", speechRoutes);
 
